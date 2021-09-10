75343cbd22d6ead1055f899f6470c29f5a62f8b9
Waqas Shabbir <waqas.shabbir@sky.uk>
2021-09-10 01:04:40 +0100

    Creating sample data to see to unit tests

---
+++ test/seed.ts
@@ -0,0 +1,10 @@
+const newNotes = [ {
+    title: "test123",
+    description: "test123 description",
+    owner: "waqas shabbir",
+    creationDate: new Date(),
+    modificationDate: new Date()
+} ];
+
+
+export { newNotes};
